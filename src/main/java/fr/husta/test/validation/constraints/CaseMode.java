474f9f5fd6be7679034efb7126403b9e9ebf8bb3
ghusta <guillaume.husta@douane.finances.gouv.fr>
2013-06-19 16:45:30 +0200

    Add custom constraint : @CheckCase

---
+++ src/main/java/fr/husta/test/validation/constraints/CaseMode.java
@@ -0,0 +1,16 @@
+package fr.husta.test.validation.constraints;
+
+public enum CaseMode
+{
+
+    /**
+     * Upper case mode
+     */
+    UPPER,
+
+    /**
+     * Lower case mode
+     */
+    LOWER;
+
+}
