1ba0619b559ee817a1c6890f524aabc25fbcd5a4
ghusta <guillaume.husta@douane.finances.gouv.fr>
2014-11-04 16:14:05 +0100

    Testing @Min and @Max

--- src/main/java/fr/husta/test/ex1/MonBean.java
+++ src/main/java/fr/husta/test/ex1/MonBean.java
@@ -1,5 +1,7 @@
 package fr.husta.test.ex1;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,6 +13,10 @@ public class MonBean
     @Range(min = 0, max = 200)
     private int age;
 
+    @Min(value = 1)
+    @Max(value = 200)
+    private int age2;
+
     @NotNull(message = "Nom obligatoire")
     @Size(min = 1, max = 10)
     private String name;
@@ -20,7 +26,6 @@ public class MonBean
 
     public MonBean()
     {
-        // TODO Auto-generated constructor stub
     }
 
     public int getAge()
@@ -53,4 +58,13 @@ public class MonBean
         this.firstName = firstName;
     }
 
+    public int getAge2()
+    {
+        return age2;
+    }
+
+    public void setAge2(int age2)
+    {
+        this.age2 = age2;
+    }
 }
--- src/test/java/fr/husta/test/ex1/MonBeanValidationTest.java
+++ src/test/java/fr/husta/test/ex1/MonBeanValidationTest.java
@@ -1,13 +1,13 @@
 package fr.husta.test.ex1;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-
-import junit.framework.Assert;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -38,31 +38,34 @@ public class MonBeanValidationTest
         // TEST #1
         MonBean monBean1 = new MonBean();
         monBean1.setAge(2);
+        monBean1.setAge2(0);
         monBean1.setName("toto");
 
         constraintViolations = validator.validate(monBean1);
-        Assert.assertTrue(constraintViolations.size() == 0);
+        assertTrue(constraintViolations.size() == 1);
 
         System.out.println(constraintViolations);
 
         // TEST #2
         MonBean monBean2 = new MonBean();
         //        monBean1.setAge(2);
+        monBean1.setAge2(1);
         //        monBean1.setName("toto");
 
         constraintViolations = validator.validate(monBean2);
-        Assert.assertTrue(constraintViolations.size() != 0);
+        assertTrue(constraintViolations.size() == 2);
 
         System.out.println(constraintViolations);
 
         // TEST #3
         MonBean monBean3 = new MonBean();
         monBean3.setAge(2);
+        monBean3.setAge2(2);
         monBean3.setName("toto");
         monBean3.setFirstName(null);
 
         constraintViolations = validator.validate(monBean3, Groupe1Checks.class);
-        Assert.assertTrue(constraintViolations.size() != 0);
+        assertTrue(constraintViolations.size() == 1);
 
         System.out.println(constraintViolations);
 
