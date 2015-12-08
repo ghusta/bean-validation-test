3fb030cbb9350d1edce1dec5678ccf0c66049005
Guillaume Husta <g.husta@free.fr>
2015-12-08 23:16:48 +0100

    Add custom constraint : @MonetaryAmount & @MonetaryAmountPositive

---
+++ src/main/java/fr/husta/test/validation/constraints/money/MonetaryAmount.java
@@ -0,0 +1,34 @@
+package fr.husta.test.validation.constraints.money;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.Digits;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Check monetary amount : 16 integer digits, 2 decimal max.
+ * <p/>
+ * See also <a href="https://jcp.org/en/jsr/detail?id=354">JSR 354: Money and Currency API</a>.
+ */
+@Digits(integer = 16, fraction = 2)
+@ReportAsSingleViolation
+@Target({METHOD, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+public @interface MonetaryAmount {
+
+    String message() default "{fr.husta.test.validation.constraints.MonetaryAmount.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/fr/husta/test/validation/constraints/money/MonetaryAmountPositive.java
@@ -0,0 +1,33 @@
+package fr.husta.test.validation.constraints.money;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.Digits;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Check monetary amount : 16 integer digits, 2 decimal max; must be positive.
+ */
+@MonetaryAmount
+@ReportAsSingleViolation
+@Target({METHOD, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+public @interface MonetaryAmountPositive {
+
+    String message() default "{fr.husta.test.validation.constraints.MonetaryAmountPositive.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
