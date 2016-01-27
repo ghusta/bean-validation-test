e50f5083afa32c42d224d24e59c60a13830f1982
Guillaume Husta <g.husta@free.fr>
2016-01-27 23:39:14 +0100

    Testing Bean-Validation with Joda-Time

---
+++ src/main/java/fr/husta/test/ex10_jodatime/PersonWithJodaTime.java
@@ -0,0 +1,59 @@
+package fr.husta.test.ex10_jodatime;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Past;
+
+public class PersonWithJodaTime {
+
+    private String name;
+
+    private String firstName;
+
+    @Past
+    private LocalDate dob;
+
+    @Future
+    private DateTime dateTime;
+
+
+    public PersonWithJodaTime() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(DateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+}
+
+
---
+++ src/test/java/fr/husta/test/ex10_jodatime/PersonWithJodaTimeTest.java
@@ -0,0 +1,58 @@
+package fr.husta.test.ex10_jodatime;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Locale;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+
+public class PersonWithJodaTimeTest {
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void initGlobal() {
+        // set default locale to FR -> UTF-8 problem with IntelliJ ?!?
+        Locale.setDefault(Locale.ENGLISH);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void testDob_OK() throws Exception {
+        Set<ConstraintViolation<PersonWithJodaTime>> constraintViolations = null;
+
+        PersonWithJodaTime myPojo = new PersonWithJodaTime();
+        myPojo.setName("TOTO");
+        myPojo.setDob(LocalDate.parse("1995-08-31"));
+        myPojo.setDateTime(DateTime.now().plusDays(15));
+
+        constraintViolations = validator.validate(myPojo);
+
+        assertTrue(constraintViolations.size() == 0);
+    }
+
+    @Test
+    public void testDob_KO() throws Exception {
+        Set<ConstraintViolation<PersonWithJodaTime>> constraintViolations = null;
+
+        PersonWithJodaTime myPojo = new PersonWithJodaTime();
+        myPojo.setName("Too young");
+        myPojo.setDob(LocalDate.parse("2015-08-31"));
+        myPojo.setDateTime(DateTime.now().minusMinutes(10));
+
+        constraintViolations = validator.validate(myPojo);
+
+        assertTrue(constraintViolations.size() >= 1);
+    }
+
+}
