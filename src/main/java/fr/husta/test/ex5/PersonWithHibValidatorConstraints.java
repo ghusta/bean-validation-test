1b81dbc4516baf4e171a9efd295dbab0cf58c33f
Guillaume HUSTA <guillaume.husta@douane.finances.gouv.fr>
2017-03-31 09:40:57 +0200

    Remove Deprecated Constraints in BV 2.0. org.hibernate.validator.constraints.[NotBlank|NotEmpty] -> javax.validation.constraints.[NotBlank|NotEmpty]

--- src/main/java/fr/husta/test/ex5/PersonWithHibValidatorConstraints.java
+++ src/main/java/fr/husta/test/ex5/PersonWithHibValidatorConstraints.java
@@ -1,9 +1,9 @@
 package fr.husta.test.ex5;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * Uses Hibernate Validator custom constraints : @NotEmpty and @NotBlank.
